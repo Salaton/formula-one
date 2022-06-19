fe404833fcb2cf04925d82c2ecd128f5e5c3edba
Salaton <nairouasalaton@gmail.com>
2022-06-19 20:58:30 +0300

    feat: implement race schedule graphql API

---
+++ pkg/presentation/graph/raceschedule.resolvers.go
@@ -0,0 +1,20 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+
+	"github.com/Salaton/formula-one/pkg/domain"
+	"github.com/Salaton/formula-one/pkg/presentation/graph/generated"
+)
+
+func (r *queryResolver) RaceSchedule(ctx context.Context, year int) (*domain.DataResponse, error) {
+	return r.Resolver.RaceSchedule.GetSeasonRaceSchedules(ctx, year)
+}
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type queryResolver struct{ *Resolver }
