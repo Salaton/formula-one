1411b0eedf7aa3d77d4cdecfa030b0fe54c196f4
Salaton <nairouasalaton@gmail.com>
2022-06-30 23:38:13 +0300

    ci: setup google cloud run deployment pipeline

--- config/config.go
+++ config/config.go
@@ -25,7 +25,9 @@ type EnvVariablesConfig struct {
 func LoadConfig() (*Configuration, error) {
 	var config *Configuration
 	viper.SetConfigFile(configFile)
+	// automatically override values that it has read from config file with the values of the corresponding environment variables if they exist.
 	viper.AutomaticEnv()
+	viper.SetDefault("PORT", 8080)
 
 	// Watch config file, no need to restart server after updating the config file
 	viper.OnConfigChange(func(e fsnotify.Event) {
@@ -33,15 +35,12 @@ func LoadConfig() (*Configuration, error) {
 	})
 	viper.WatchConfig()
 
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			return nil, fmt.Errorf("config file not found, please update the `ConfigPath`: %w", err)
-		} else {
-			return nil, fmt.Errorf("failed to read config file: %w", err)
-		}
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Err(err).Msg("failed to read config")
 	}
 
-	err := viper.Unmarshal(&config)
+	err = viper.Unmarshal(&config)
 	if err != nil {
 		return nil, fmt.Errorf("failed to unmarshal the config: %w", err)
 	}
