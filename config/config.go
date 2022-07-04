6f7a4285b5e0a137861d159baa3abe9a6f280cda
Salaton <nairouasalaton@gmail.com>
2022-07-04 23:47:10 +0300

    fix: assorted fixes
    This PR addresses the following issues
    -  pass the port via a config
    -  put the command orchestrator in the RunE field
    -  Removed the zerolog abstractions
    -  Separated the imports into standard lib, third party and own imports

--- config/config.go
+++ config/config.go
@@ -3,9 +3,8 @@ package config
 import (
 	"fmt"
 
-	"github.com/Salaton/formula-one/pkg/presentation/logger"
 	"github.com/fsnotify/fsnotify"
-	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
 
@@ -20,17 +19,17 @@ type Configuration struct {
 type EnvVariablesConfig struct {
 	ErgastAPIResponseType string `mapstructure:"ERGAST_API_RESPONSE_TYPE"`
 	ErgastAPIEndpoint     string `mapstructure:"ERGAST_API_ENDPOINT"`
+	Port                  int    `mapstructure:"PORT"`
 }
 
 func LoadConfig() (*Configuration, error) {
 	var config *Configuration
-	logger := logger.NewLogger()
 	viper.SetConfigFile(configFile)
 	viper.AutomaticEnv()
 
 	// Watch config file, no need to restart server after updating the config file
 	viper.OnConfigChange(func(e fsnotify.Event) {
-		logger.Log(zerolog.InfoLevel, "Config file changed: %v", e.Name)
+		log.Info().Msgf("config file changed: %v", e.Name)
 	})
 	viper.WatchConfig()
 
